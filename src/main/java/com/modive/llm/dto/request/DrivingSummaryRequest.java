ce876e766d620864b1da80dabb216c5a56ba30b1
rndudals <rn9dud0@naver.com>
2025-05-01 16:26:09 +0900

    refactor(#5): DTO 생성 및 패키지 위치 변경

--- src/main/java/com/modive/llm/dto/request/DrivingSummaryRequest.java
+++ src/main/java/com/modive/llm/dto/request/DrivingSummaryRequest.java
@@ -1,11 +1,9 @@
-package com.modive.llm.dto;
+package com.modive.llm.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @Builder
